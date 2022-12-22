20397b70cee85b71dd558d782580cc789cf2404e
laurapee <105225852+laurapee@users.noreply.github.com>
2022-12-22 21:10:52 +0100

    Ergänzung Ausstelung

--- Ausstellung.java
+++ Ausstellung.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 /**
  * Eine Ausstellung enthält jeweils die in dieser Ausstellung präsentierten Kunstwerke und die Aufteilung der Kunstwerke in die einzelnen Räume. Eine 
@@ -15,31 +17,27 @@ public class Ausstellung
     // a) zusätzliche Attribute der Klasse Kunstwerk (wirdAusgestellt + ausstellungsraum)
     // b) mehrdimensionales Array mit Kunstwerk + Raumzuordnung
     
-    private ArrayList<Kunstwerk> ausstellung;    
-    Datei d;
+    private HashMap<Kunstwerk, Raum> ausstellung;
+    
+    
     /**
      * Konstruktor für Objekte der Klasse Ausstellung
+     * @param Übergeben wird die Zuordnung von Kunstwerk zu Raum, 
+     * welche im Optimierungsalgorithmus erstellt wird
      */
-    public Ausstellung(ArrayList<Kunstwerk> kunstwerke)
+    public Ausstellung(HashMap<Kunstwerk, Raum> ausstellung)
     {
-        // Instanzvariable initialisieren
-        ausstellung = new ArrayList<Kunstwerk>();
-        
-        /*for()
-        {
-            //jedes Objekt aus kunstwerke
-            //prüfen, ob wirdAusgestellt
-            //append an ausstellung
-        }*/
+
+        this.ausstellung = ausstellung;
+
     }
 
     /**
      * Um die Ausstellung aus anderen Klassen aufrufen zu können, wird eine get-Methode definiert
      * 
-     * @param  y    ein Beispielparameter für eine Methode
      * @return       die Ausstellung
      */
-    public ArrayList<Kunstwerk> getAustellung()
+    public HashMap<Kunstwerk, Raum> getAustellung()
     {
         // tragen Sie hier den Code ein
         return ausstellung;
